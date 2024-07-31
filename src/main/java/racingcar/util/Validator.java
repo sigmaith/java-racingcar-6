21b5a219e0907176557ed52c3b2fa21fef0bb11a
Smith <125195487+sigmaith@users.noreply.github.com>
2024-07-31 14:28:42 +0900

    feat(validator): 입력 검증 기능

---
+++ src/main/java/racingcar/util/Validator.java
@@ -0,0 +1,29 @@
+package racingcar.util;
+
+import static java.util.Arrays.stream;
+
+public class Validator {
+    private static final String DELIMITER = ",";
+    private static final String POSITIVE_INTEGER_REGEX = "^[1-9]\\\\d*$";
+
+    public static String[] validateCarNames(String carNames) {
+        String[] carNameArray = carNames.split(DELIMITER);
+        if (!areCarNamesValid(carNameArray)) {
+            throw new IllegalArgumentException("자동차 이름은 쉼표로 구분하여 비어 있지 않고 1자에서 5자 사이여야 합니다.");
+        }
+        return carNameArray;
+    }
+
+    public static int validateAttemptCount(String attemptCount) {
+        if (!attemptCount.matches(POSITIVE_INTEGER_REGEX)) {
+            throw new IllegalArgumentException("시도 횟수는 양의 정수여야 합니다.");
+        }
+        return Integer.parseInt(attemptCount);
+    }
+
+    private static boolean areCarNamesValid(String[] carNameArray) {
+        return carNameArray.length > 0 &&
+                stream(carNameArray)
+                        .allMatch(carName -> carName.length() >= 1 && carName.length() <= 5);
+    }
+}
